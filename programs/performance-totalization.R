c20a9d90f356278689bfe167729b63eec1492041
kazuminmin <kazumi.takeuchi@nnh.go.jp>
2018-01-10 15:02:52 +0900

    完成

--- programs/performance-totalization.R
+++ programs/performance-totalization.R
@@ -3,10 +3,18 @@
 # 作成者: mamiko yonejima
 # 作成日: 2017/12/26
 # 作成者: kazumi takeuchi, mamiko yonejima
-# 定モニのスタイル変更に伴う変更
+# 変更内容：定モニのスタイル変更に伴う変更
 
+# 関数の設定
+# ｂｙででできた集計値をデータフレームに変換する関数
+ConvertDataframe <- function(dataframe){
+  x <- as.vector(dataframe)
+  試験施設名 <- names(dataframe)
+  data.frame(試験施設名,　x)
+}
 
-# 設定部分
+# config
+# pathの設定
 prtpath <- "//192.168.200.222/Datacenter/学会事務/230_月１登録状況DM確認用/Ptosh/2017年/20171201/performance"
 
 # rawdataのリストを作成
@@ -26,80 +34,31 @@ submit_sheet <- by(allfiles$送信シート数,allfiles$CMTRT_HP, sum)
 # 試験施設名毎に督促中シート数毎にsum
 demand_sheet <- by(allfiles$督促中シート数,allfiles$CMTRT_HP, sum)
 
-# ｂｙででできた集計値をデータフレームに変換する関数
-ConvertDataframe <- function(dataframe){  #Todoyonejima
-  x <- as.vector(dataframe)
-  試験施設名 <- names(dataframe)
-  data.frame(試験施設名,x)
-}
-
+# ｂｙででできた集計値をデータフレームに変換
 df_regi <- ConvertDataframe(registration_number)
 df_submit <- ConvertDataframe(submit_sheet)
 df_demand <- ConvertDataframe(demand_sheet)
 
-#merge
-merge_all <- merge(df_regi,df_submit,by="試験施設名",all=T)
-merge_all1 <- merge(merge_all,df_demand,by="試験施設名",all=T)
+# merge
+merge_all <- merge(df_regi, df_submit,by = "試験施設名", all = T)
+ads0 <- merge(merge_all, df_demand,by = "試験施設名", all = T)
 
-#変数名変更
-names(merge_all1)[2:4] <- c("症例登録数","送信シート数","督促中シート数")
+# 変数名変更
+names(ads0)[2:4] <- c("症例登録数","送信シート数","督促中シート数")
 
 # 試験名と施設名で分ける
-# 試験名をforで回すためにリストにする levels(allfiles$試験名)
+ads0$試験名 <- sub("_.*", "", ads0$試験施設名)
+ads0$施設名 <- sub("^.*._", "", ads0$試験施設名)
 
+# 必要列のみ抽出
+ads <- ads0[,c(5, 6, 2:4)]
 
-# 症例登録数<- as.vector(registration_number)
-# 試験施設名 <- names(registration_number)
-#
-# z <- data.frame(
-#   試験施設名,症例登録数
-# )
-#
-# 送信シート数<- as.vector(demand_sheet)
-# 試験施設名 <- names(demand_sheet)
-#
-# z1 <- data.frame(
-#   試験施設名,送信シート数
-# )
+# 試験名をforで回すためにリストにする
+list_trial <- levels(allfiles$試験名)
 
-
-# # 今日の日付
-# Today 　<-  "20170727"
-# setwd("../rawdata")
-#
-# # Making List
-# list <- as.data.frame(list.files())
-# list$no  <- c(1:nrow(list))
-# list$df <- paste0("file",list$no)
-# colnames(list) <- c("file","no","name")
-#
-# # ファイルの読み込み
-# for(i in 1:length(list$no)){
-#   eval(parse(text = paste0("file", list$no[i], " <- read.csv('", list$file[i], "', as.is = T)")))
-# }
-#
-# #ファイルのバイント
-# filename <- paste(list$name, sep="", collapse=",")
-#
-# allData <- eval(
-#   parse(
-#     text=paste0("rbind(",filename,")")
-#   )
-# )
-# #集計
-# result1  <-capture.output(by(allData$送信シート数,allData$試験名,sum))
-# result2 <-capture.output(by(allData$督促中シート数,allData$試験名,sum))
-# #結果の書き出し
-# setwd("../output")
-# eval(
-#   parse(
-#     text=paste0("write.csv(result1,'送信シート数",Today,".csv',row.names = F)")
-#   )
-# )
-# eval(
-#   parse(
-#     text=paste0("write.csv(result2,'督促中シート数",Today,".csv',row.names = F)")
-#   )
-# )
-
-
+# データ出力
+setwd("../output")
+for(i in 1:length(list_trial)){
+  ds<- ads[ads$試験名==list_trial[i],]
+  write.csv(ds, paste0(list_trial[i], "_performance.csv" ), row.names = F)
+}
