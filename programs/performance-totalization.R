47279c0da612f68cea6d5119f6bae61637e07f33
ma3k0 <mamiko.yonejima@nnh.go.jp>
2018-06-25 15:14:38 +0900

    "治療状況調査未回答症例数"が追加となったので、コードを修正した。

--- programs/performance-totalization.R
+++ programs/performance-totalization.R
@@ -15,49 +15,52 @@ ConvertDataframe <- function(dataframe){
 
 # config
 # pathの設定
-prtpath <- "//192.168.200.222/Datacenter/学会事務/230_月１登録状況DM確認用/Ptosh/2017年/20171201/performance"
+prtpath <- "//192.168.200.222/Datacenter/学会事務/230_月１登録状況DM確認用/Ptosh/2018年/20180601/performance"
 
 # rawdataのリストを作成
 file_list <- list.files(paste0(prtpath, "/rawdata"))
 setwd(paste0(prtpath, "/rawdata"))
 allfiles <- Reduce(rbind, lapply(file_list,  read.csv)) # バインドしながらリストをすべて読み込み
-
+allfiles[is.na(allfiles)] <- 0
 # 試験名と施設名を繋ぐ変数の導出（CMTRT_HP）
 allfiles$CMTRT_HP <- paste0(allfiles$試験名, "_", allfiles$施設科名)
 
 # 試験施設名毎に症例登録数をsum
-registration_number <- by(allfiles$症例登録数,allfiles$CMTRT_HP, sum)
+registration_number <- by(allfiles$症例登録数, allfiles$CMTRT_HP, sum)
 
 # 試験施設名毎に送信シート数毎にsum
-submit_sheet <- by(allfiles$送信シート数,allfiles$CMTRT_HP, sum)
+submit_sheet <- by(allfiles$送信シート数, allfiles$CMTRT_HP, sum)
 
 # 試験施設名毎に督促中シート数毎にsum
-demand_sheet <- by(allfiles$督促中シート数,allfiles$CMTRT_HP, sum)
+demand_sheet <- by(allfiles$督促中シート数, allfiles$CMTRT_HP, sum)
+
+# 試験施設名毎に治療状況調査未回答症例数毎にsum
+investigation <- by(allfiles$治療状況調査未回答症例数, allfiles$CMTRT_HP, sum)
 
 # ｂｙででできた集計値をデータフレームに変換
 df_regi <- ConvertDataframe(registration_number)
 df_submit <- ConvertDataframe(submit_sheet)
 df_demand <- ConvertDataframe(demand_sheet)
-
-# merge
-merge_all <- merge(df_regi, df_submit,by = "試験施設名", all = T)
-ads0 <- merge(merge_all, df_demand,by = "試験施設名", all = T)
-
-# 変数名変更
-names(ads0)[2:4] <- c("症例登録数","送信シート数","督促中シート数")
+df_investigation <- ConvertDataframe(investigation)
+# mergeと変数名変更
+merge1 <- merge(df_regi, df_submit, by = "試験施設名", all = T)
+merge2 <- merge(merge1, df_demand, by = "試験施設名", all = T)
+names(merge2)[2:4] <- c("症例登録数","送信シート数","督促中シート数")
+ads0 <- merge(merge2, df_investigation, by = "試験施設名", all = T)
+names(ads0)[5] <- "治療状況調査未回答症例数"
 
 # 試験名と施設名で分ける
 ads0$試験名 <- sub("_.*", "", ads0$試験施設名)
 ads0$施設名 <- sub("^.*._", "", ads0$試験施設名)
 
 # 必要列のみ抽出
-ads <- ads0[,c(5, 6, 2:4)]
+ads <- ads0[,c(6, 7, 2:5)]
 
 # 試験名をforで回すためにリストにする
 list_trial <- levels(allfiles$試験名)
 
 # データ出力
-setwd("../output")
+setwd(paste0(prtpath, "/output"))
 for(i in 1:length(list_trial)){
   ds<- ads[ads$試験名==list_trial[i],]
   write.csv(ds, paste0(list_trial[i], "_performance.csv" ), row.names = F)
