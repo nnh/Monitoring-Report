4ac163bb591ac9f0f35f6a9e2bbf683456b2a49e
ma3k0 <mamiko.yonejima@nnh.go.jp>
2022-05-30 16:22:21 +0900

    再アップ

--- programs/plot.R
+++ programs/plot.R
@@ -5,7 +5,7 @@
 # config*******
 prtpath <- "C:/Users/MamikoYonejima/Box/Datacenter/Trials/JPLSG/49_ALL-B19/10.03.10 データレビュー書/第1回/データクリーニング"
 kTrialTitle  <- "ALL-B19"
-
+kName <- "WBC"
 #**************
 kToday <- Sys.Date()
 library(tidyverse)
@@ -14,8 +14,8 @@ library(tidyverse)
 rawdatapath <- paste0(prtpath, "/rawdata/")
 lb <- read_csv(paste0(rawdatapath, "LB.csv"))
 
-lb_dxt <-lb[lb$LBTESTCD =="WBC", ]
+lb_dxt <-lb[lb$LBTESTCD == kName , ]
 
-
-plot(lb_dxt$LBSEQ, lb_dxt$LBORRES)
+plot <- plot(lb_dxt$LBSEQ, lb_dxt$LBORRES, xlab = "", ylab = kName, xaxt="n")
+png("plot1.png", width = 100, height = 100)
 
